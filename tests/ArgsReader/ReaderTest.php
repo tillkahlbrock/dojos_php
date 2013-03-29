f24c8c461e90973c47ece5ad802cec7af535531d
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-03-29 17:17:51 +0100

    refactors ReaderTest

--- tests/ArgsReader/ReaderTest.php
+++ tests/ArgsReader/ReaderTest.php
@@ -2,13 +2,20 @@
 
 class ReaderTest extends PHPUnit_Framework_TestCase
 {
+
+    private $specification;
+
+    public function setUp()
+    {
+        $this->specification = array();
+    }
+
     /**
      * @test
      */
     public function it_should_return_true_for_an_empty_argslist_if_no_specification_is_given()
     {
-       $reader = new \ArgsReader\Reader(array());
-       $this->assertTrue($reader->parse(array()));
+       $this->assertTrue($this->buildReader()->parse(array()));
     }
 
     /**
@@ -16,12 +23,11 @@ class ReaderTest extends PHPUnit_Framework_TestCase
      */
     public function it_should_return_true_if_parameter_f_is_specified_but_not_given()
     {
-        $spec = array(
+        $this->specification = array(
             'f' => 'boolean'
         );
 
-        $reader = new \ArgsReader\Reader($spec);
-        $this->assertTrue($reader->parse(array()));
+        $this->assertTrue($this->buildReader()->parse(array()));
     }
 
     /**
@@ -29,13 +35,21 @@ class ReaderTest extends PHPUnit_Framework_TestCase
      */
     public function it_should_return_the_default_value_false_for_a_boolean_parameter()
     {
-        $spec = array(
+        $this->specification = array(
             'f' => 'boolean'
         );
 
-        $reader = new \ArgsReader\Reader($spec);
+        $reader = $this->buildReader();
         $reader->parse(array());
 
         $this->assertFalse($reader->getArg('f'));
+    }
+
+    /**
+     * @return \ArgsReader\Reader
+     */
+    private function buildReader()
+    {
+        return new \ArgsReader\Reader($this->specification);
     }
 }
