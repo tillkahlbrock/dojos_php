cc16ea7bb6284f4f0967b1a6dc3e22da5fa9bec6
Till Kahlbrock <till@jimdo.com>
2013-04-02 09:27:35 +0200

    wip: integrates parser and reader

--- src/ArgsReader/Reader.php
+++ src/ArgsReader/Reader.php
@@ -4,21 +4,15 @@ namespace ArgsReader;
 
 class Reader
 {
-    public function __construct($specification)
+    private $parser;
+
+    public function __construct(\ArgsReader\Parser $parser)
     {
-        $this->specification = $specification;
+        $this->parser = $parser;
     }
 
     public function read($parameterString)
     {
-        $parser = new \ArgsReader\Parser();
-        $paramMapping = $parser->parse($parameterString);
-        $this->validate($paramMapping);
-        $this->paramMapping = $paramMapping;
-    }
-
-    public function getParam($key)
-    {
-        return $this->paramMapping[$key];
+        $this->parser->parse($parameterString);
     }
 }
--- tests/ArgsReader/ReaderTest.php
+++ tests/ArgsReader/ReaderTest.php
@@ -2,5 +2,21 @@
 
 class ReaderTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     */
+    public function it_should_call_the_parser_with_the_given_parameter_string()
+    {
+        $paramString = 'some param string';
 
+        $parser = $this->getMock('\ArgsReader\Parser');
+        $parser
+            ->expects($this->once())
+            ->method('parse')
+            ->with($paramString);
+
+        $reader = new \ArgsReader\Reader($parser);
+
+        $reader->read($paramString);
+    }
 }
