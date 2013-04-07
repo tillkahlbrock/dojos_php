a24f4168979d75c9a4b42a9cd2daea19cff09586
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-04-07 22:12:04 +0200

    wip: extracts validator

--- src/ArgsReader/Reader.php
+++ src/ArgsReader/Reader.php
@@ -5,14 +5,24 @@ namespace ArgsReader;
 class Reader
 {
     private $parser;
+    private $validator;
 
-    public function __construct(\ArgsReader\Parser $parser)
+    public function __construct(
+        \ArgsReader\Parser $parser,
+        \ArgsReader\Validator $validator
+    )
     {
         $this->parser = $parser;
+        $this->validator = $validator;
     }
 
     public function read($parameterString)
     {
-        $this->parser->parse($parameterString, array());
+        $paramArgMapping = $this->parser->parse($parameterString, array());
+        if (!$this->validator->isValid($paramArgMapping)) {
+            return $this->validator->getErrors();
+        }
+
+        return $paramArgMapping;
     }
 }
