bbc07e3326e8b51a88783463ace9a830275fa2a3
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-04-08 21:43:43 +0200

    implements getArgument

--- src/ArgsReader/Reader.php
+++ src/ArgsReader/Reader.php
@@ -6,6 +6,7 @@ class Reader
 {
     private $parser;
     private $validator;
+    private $parameters;
 
     public function __construct(
         \ArgsReader\Parser $parser,
@@ -14,13 +15,26 @@ class Reader
     {
         $this->parser = $parser;
         $this->validator = $validator;
+        $this->parameters = array();
     }
 
     public function read($parameterString)
     {
         $paramArgMapping = $this->parser->parse($parameterString, array());
+
         if (!$this->validator->validate($paramArgMapping)) {
             throw new \InvalidArgumentException('Validation failed');
         }
+
+        $this->parameters = $paramArgMapping;
+    }
+
+    public function getArgument($flag)
+    {
+        if (isset($this->parameters[$flag])) {
+            return $this->parameters[$flag];
+        }
+
+        return null;
     }
 }
--- tests/ArgsReader/ReaderTest.php
+++ tests/ArgsReader/ReaderTest.php
@@ -3,6 +3,8 @@
 class ReaderTest extends PHPUnit_Framework_TestCase
 {
     const SOME_PARAM_STRING = 'some param string';
+    const SOME_FLAG = 'f';
+    const SOME_ARGUMENT = 'an argument';
 
     /**
      * @var \ArgsReader\Parser | PHPUnit_Framework_MockObject_MockObject
@@ -85,9 +87,54 @@ class ReaderTest extends PHPUnit_Framework_TestCase
         $this->buildReader()->read(self::SOME_PARAM_STRING);
     }
 
+    /**
+     * @test
+     */
+    public function it_should_return_null_if_the_requested_flag_has_not_been_parsed()
+    {
+        $this->validator
+            ->expects($this->any())
+            ->method('validate')
+            ->will($this->returnValue(true));
+
+        $this->parser
+            ->expects($this->any())
+            ->method('parse')
+            ->will($this->returnValue(array('f' => 'an argument')));
+
+        $reader = $this->buildReader();
+
+        $reader->read(self::SOME_PARAM_STRING);
+        $this->assertNull($reader->getArgument('g'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_the_value_for_the_requested_flag()
+    {
+        $flag = self::SOME_FLAG;
+        $argument = self::SOME_ARGUMENT;
+
+        $this->validator
+            ->expects($this->any())
+            ->method('validate')
+            ->will($this->returnValue(true));
+
+        $this->parser
+            ->expects($this->any())
+            ->method('parse')
+            ->will($this->returnValue(array($flag => $argument)));
+
+        $reader = $this->buildReader();
+
+        $reader->read(self::SOME_PARAM_STRING);
+        $this->assertEquals($argument, $reader->getArgument($flag));
+    }
+
     private function someMapping()
     {
-        return array('f' => 'some_arg');
+        return array(self::SOME_FLAG => self::SOME_ARGUMENT);
     }
 
     /**
