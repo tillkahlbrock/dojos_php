5395a2761e31c83092a58fb7f378ecf67b1e8f2f
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-04-13 20:15:33 +0200

    adds scoring for first player (kind of...)

--- src/Tennis/Match.php
+++ src/Tennis/Match.php
@@ -4,8 +4,23 @@ namespace Tennis;
 
 class Match
 {
+    public function __construct()
+    {
+        $this->scores = array(0,0);
+    }
+
+    public function scoreFirstPlayer()
+    {
+        $this->scores[0] += 1;
+    }
+
     public function getScores()
     {
-        return '0 : 0';
+        $scoreMap = array(
+            0 => '0',
+            1 => '15'
+        );
+
+        return $scoreMap[$this->scores[0]] . ' : ' . $scoreMap[$this->scores[1]];
     }
 }
--- tests/Tennis/MatchTest.php
+++ tests/Tennis/MatchTest.php
@@ -7,8 +7,23 @@ class MatchTest extends PHPUnit_Framework_TestCase
      */
     public function it_should_have_no_points_in_a_just_started_match()
     {
-        $match = new \Tennis\Match();
+        $this->assertEquals('0 : 0', $this->buildMatch()->getScores());
+    }
 
-        $this->assertEquals('0 : 0', $match->getScores());
+    /**
+     * @test
+     */
+    public function it_should_return_15_to_zero_points_if_first_player_scores_once()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+
+        $this->assertEquals('15 : 0', $match->getScores());
+    }
+
+    private function buildMatch()
+    {
+        return new \Tennis\Match();
     }
 }
