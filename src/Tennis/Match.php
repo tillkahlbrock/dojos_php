9e09aa91165298b38c0434192fc404650d5617f2
Till Kahlbrock <till@jimdo.com>
2013-04-18 12:00:39 +0200

    adds more features

--- src/Tennis/Match.php
+++ src/Tennis/Match.php
@@ -14,13 +14,38 @@ class Match
         $this->scores[0] += 1;
     }
 
+    public function scoreSecondPlayer()
+    {
+        $this->scores[1] += 1;
+    }
+
     public function getScores()
     {
         $scoreMap = array(
             0 => '0',
-            1 => '15'
+            1 => '15',
+            2 => '30',
+            3 => '40'
         );
 
-        return $scoreMap[$this->scores[0]] . ' : ' . $scoreMap[$this->scores[1]];
+        if ($this->scores[0] < 3 || $this->scores[1] < 3) {
+            return $scoreMap[$this->scores[0]] . ' : ' . $scoreMap[$this->scores[1]];
+        }
+
+        if ($this->scores[0] == $this->scores[1]) {
+            return 'deuce';
+        }
+
+        if ($this->scores[0] == ($this->scores[1] + 1)) {
+            return 'advantage player1';
+        }
+
+        if ($this->scores[0] == ($this->scores[1] - 2)) {
+            return 'player2 wins';
+        }
+
+        if ($this->scores[0] == ($this->scores[1] - 1)) {
+            return 'advantage player2';
+        }
     }
 }
--- tests/Tennis/MatchTest.php
+++ tests/Tennis/MatchTest.php
@@ -22,6 +22,148 @@ class MatchTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('15 : 0', $match->getScores());
     }
 
+    /**
+     * @test
+     */
+    public function it_should_return_15_to_15_points_if_both_players_score_once()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreSecondPlayer();
+
+        $this->assertEquals('15 : 15', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_30_points_for_first_player_if_he_scores_twice()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $this->assertEquals('30 : 0', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_40_points_for_first_player_if_he_scores_three_times()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $this->assertEquals('40 : 0', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_deuce_if_both_players_score_three_times()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+
+        $this->assertEquals('deuce', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_advantage_player1_if_player1_scores_once_after_deuce()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+
+        $match->scoreFirstPlayer();
+
+        $this->assertEquals('advantage player1', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_advantage_player2_if_player2_scores_once_after_deuce()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+
+        $match->scoreSecondPlayer();
+
+        $this->assertEquals('advantage player2', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_deuce_if_player1_scores_once_after_player2_had_advantage()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+
+        $match->scoreSecondPlayer();
+
+        $match->scoreFirstPlayer();
+
+        $this->assertEquals('deuce', $match->getScores());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_player2_wins_if_player2_scores_once_after_he_had_advantage()
+    {
+        $match = $this->buildMatch();
+
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+        $match->scoreFirstPlayer();
+
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+        $match->scoreSecondPlayer();
+
+        $match->scoreSecondPlayer();
+
+        $match->scoreSecondPlayer();
+
+        $this->assertEquals('player2 wins', $match->getScores());
+    }
+
     private function buildMatch()
     {
         return new \Tennis\Match();
